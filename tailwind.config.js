f476f0c9f6abd33265a7efc541fbaecbeff16cb9
nxtrm <nextrem_p@proton.me>
2024-08-12 15:24:37 +0300

    Adjust tailwind config and style the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#49SE57",  // The green color from the palette
-        secondary: "#F4CE14", // The yellow color from the palette
-        neutralDark: "#495E57", // Darker neutral color
-        neutralLight: "#EDEFEE", // Lighter neutral color
+        primaryGreen: "rgb(73,94,87)",  // The green color from the palette
+        primaryYellow: 'rgb(244,206,20)', // The yellow color from the palette
+        secondaryTomato: 'rgb(238,153,114)',
+        secondaryBeige: "rgb(251, 218, 187)",
+        highlightDark: "rgb(51, 51, 51)", // Darker highlight color
+        highlightLight: "rgb(237, 219, 238)", // Lighter highlight color
       },
       fontFamily: {
         heading: ["'Markazi Text'", "serif"], // Display and heading font
         body: ["'Karla'", "sans-serif"], // Body text font
       },
       fontSize: {
-        display: "40pt", // Display text size
-        subtitle: "40pt", // Subtitle size, similar to the display size
-        lead: "18pt", // Lead text size
-        cardTitle: "18pt", // Card title size
-        paragraph: "16pt", // Paragraph text size
-        highlight: "16pt", // Highlight text size
+        display: "20pt", // Display text size
+        subtitle: "20pt", // Subtitle size, similar to the display size
+        lead: "14pt", // Lead text size
+        cardTitle: "12pt", // Card title size
+        paragraph: "10pt", // Paragraph text size
+        highlight: "10pt", // Highlight text size
       },
       borderRadius: {
         DEFAULT: "16px", // Rounded corners radius
